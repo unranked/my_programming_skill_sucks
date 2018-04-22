cd8f108c7fe1d4bfbe7fc65cde5e4591c3366fef
Стёпа .- <38585178+unranked@users.noreply.github.com>
2018-04-22 09:18:12 +0300

    Update my_pars.py

--- my_pars.py
+++ my_pars.py
@@ -18,7 +18,7 @@ def main():
 
     url = input()                           # Вводим URL
     url = ''.join(url.split())              # Избавляемся от неожиданных пробелов в данном пользователем URL
-    if not is_valid(url):                   # Проверяем, можем ли мы получить доступ к ссылке, а так же корректно ли она написана
+    if not is_valid(url):                   # Проверяем, можем ли мы получить доступ к ссылке, а также корректно ли она написана
         exit("Невозможно получить информацию с данного URL")
 
     settings = config[ans]                  # Достаём настройки для i'ого новостного сервиса
@@ -26,20 +26,20 @@ def main():
     html_doc = requests.get(url).content.decode(settings["encoding"])   # Получаю html-код из ссылки
     soup = bs4.BeautifulSoup(html_doc, 'html.parser')                   # Преобразую в элемент типа BS
 
-    tag, where = settings["where"]          # tag - тэг основного местонахожения основной информации, where - класс.      
-    content = settings["text"]              # content - то, что мы достанем из html-кода
-    table = soup.find(tag, class_=where)    # Ищем место, где это всё и расположено.
+    tag, where = settings["where"]
+    content = settings["text"]
+    table = soup.find(tag, class_=where)
 
     for temp in content:                    # Обработка ссылок:
         for row in table.find_all(temp):    # На данном этапе разработки ссылки мы просто удаляем,
             for item in row.find_all('a'):  # оставляя просто текст.
-                item = item.text            # Это можно исправить прямо в этой строчке, да..
+                item = item.text            # Но это можно быстро исправить..
 
-    raw_text = []                           # А тут просто пихаем поабзацно весь текст
+    raw_text = []
     
-    for temp in content:                    #
-        for row in table.find_all(temp):    # Точнее вот тут..
-            raw_text.append(row.text)       #
+    for temp in content:
+        for row in table.find_all(temp):
+            raw_text.append(row.text)
 
     
 
