2a5e422ac11c3d531c7eda31724b773f02d51fef
Стёпа .- <38585178+unranked@users.noreply.github.com>
2018-04-22 10:30:30 +0300

    Update format.py

--- format.py
+++ format.py
@@ -1,2 +1,23 @@
-class formater(string):
-    pass
+class Format:
+
+    text = []
+
+    def __init__(self, raw_data, set_width):
+        self.raw = raw_data
+        self.width = set_width
+
+    def done(self):
+        text = []
+        string = ""
+        for part in self.raw:
+            words = part.split()
+            for word in words:
+                if len(string) + len(word) < self.width or len(string) == 0:
+                    string = string + word + " "
+                else:
+                    text.append(string)
+                    string = word
+            text.append(string)
+            string = ""
+            text.append(string)
+        return text
